106befe27df990259c9e8f6d9792700a1d6c86fa
kaggelos <aggeloska1110@gmail.com>
2026-03-22 10:30:55 +0200

    ok

---
+++ Kaggelos/THEMA2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*na graftei sunarthsh pou na dexetai os
+orisma enan akeraio arithmo x kai na epistrefei 
+to athroisma 1/(1+1)^2+1/(1+2)^2+...+1/(1+x)^2 
+an to x einai thetikos akeraios, diaforetika na ektuponei -1*/
+int main () {
+
+
+
+
+
+
+
+
+return 0;   
+}
---
+++ Kaggelos/THEMATA 19-20/THEMA1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*na zhta apo ton xrhsth 20 
+akeraious thetikous arithmous
+kai na tous apothikeuei se enan 
+pinaka a, na zhta apo ton xrhsth 
+allous 20 akeraious arithmous kai 
+na tous apothikeuei se enan pinaka b.
+Na upologizei to athroisma ton 2 pinakwn
+kai na to apothikeueis se enan pinaka c.
+Na emfanizei to athroisma tou megaluterou
+kai tou mikroterou kai ton logo tous apo ton pinaka c */
